4650ef0d83e856607fdd65ec0afd8665fccf9294
Tanja Engel <tanja.engel@hotmail.ch>
2024-10-30 11:59:47 +0000

    build staging sources

--- models/staging/raw/stg_raw__facebook.sql
+++ models/staging/raw/stg_raw__facebook.sql
@@ -11,8 +11,8 @@ renamed as (
     select
         date_date,
         paid_source,
-        campaign_key,
-        campgn_name AS campaign_name,
+        campaign_key AS campaign_key,
+        campgn_name,
         CAST(ads_cost AS float64) AS ads_cost,
         impression,
         click
